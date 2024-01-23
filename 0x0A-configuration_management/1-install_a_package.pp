0d92d909eb20fdf1264659c8c0561c6d45683375
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-01-23 16:20:09 +0200

    Apply bug fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install flask using pip3
 
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
