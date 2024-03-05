ff2c7002ce61faf59842eb76b7fd3113630f4cde
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-03-05 22:19:53 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""Module that defines code that returns the number of subscribers"""
+"""Returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given sub"""
+    """Returns the number of subscribers"""
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0)\
             Gecko/20100101 Firefox/120.0"
     }
-    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
-                            headers=headers)
-    try:
-        return response.json()["data"]["subscribers"]
-    except KeyError:
+
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json",
+        headers=headers,
+        allow_redirects=False,
+    )
+
+    if response.status_code != 200:
         return 0
+
+    return response.json()["data"]["subscribers"]
