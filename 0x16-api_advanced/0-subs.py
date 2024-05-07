81fe47b2e25ca7f6194272efe01a8d6b63ef1cf8
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-05-08 00:30:30 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,16 +11,23 @@ def number_of_subscribers(subreddit: str) -> int:
     Function that returns the number of subscribers for a given subreddit.
     """
 
+    assert isinstance(subreddit, str)
+
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) \
             Gecko/20100101 Firefox/123.0"
     }
 
     response = requests.get(
-        f"http://www.reddit.com/r/{subreddit}/about.json", headers=headers
+        f"http://www.reddit.com/r/{subreddit}/about.json",
+        headers=headers,
+        allow_redirects=False,
     )
 
     if response.status_code != 200:
         return 0
 
-    return response.json().get("data", {}).get("subscribers", 0)
+    try:
+        return response.json().get("data", {}).get("subscribers", 0)
+    except ValueError:
+        return 0
